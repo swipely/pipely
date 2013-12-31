9d91eb464750fe17a40f1730d1e53211a42143af
Matt Gillooly <matt@swipely.com>
2013-12-31 13:42:36 -0500

    supply fake Fog credentials for test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 require 'timecop'
+require 'fog'
+
+Fog.credentials = {
+  aws_access_key_id: "xxx",
+  aws_secret_access_key: "xxx"
+}
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
