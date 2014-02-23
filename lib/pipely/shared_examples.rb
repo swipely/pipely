77e42a14e59813db98f7bbe772eeca41d472a4ba
Matt Gillooly <matt@swipely.com>
2014-02-23 18:08:43 -0500

    don't allow duplicate ids in rendered definitions

--- lib/pipely/shared_examples.rb
+++ lib/pipely/shared_examples.rb
@@ -17,4 +17,11 @@ shared_examples "a renderable template" do |environment, config|
   it "produces valid JSON" do
     expect(JSON.parse(rendered_json)).to be
   end
+
+  it "does not contain objects with duplicate ids" do
+    objects = JSON.parse(rendered_json)['objects']
+    distinct_ids = objects.map{|h| h['id']}.uniq.compact
+
+    expect(objects.count).to eq(distinct_ids.count)
+  end
 end
