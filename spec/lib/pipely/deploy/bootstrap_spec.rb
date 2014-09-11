97026c61d66ec978aa752d44f09fbc536b5e093f
Michael Guymon <michaelguymon@swipely.com>
2014-09-11 15:16:09 -0400

    use Bundler locked sources to package gems

--- spec/lib/pipely/deploy/bootstrap_spec.rb
+++ spec/lib/pipely/deploy/bootstrap_spec.rb
@@ -13,6 +13,29 @@ module Pipely
        s3.buckets['a-test-bucket']
     end
 
+    let(:bundled_gems) { subject.gems_from_bundler }
+    let(:build_and_upload_gems) do
+      objects = double(:objects)
+      s3_object = double('s3_object', write: nil, exists?: true)
+
+      allow(objects).to receive(:[]) { s3_object }
+
+      gems = bundled_gems.each do |name, gem|
+        unless gem.match(/^json-\d+\.\d+\.\d+\.gem$/) or
+               gem.match(/^bundler-\d+\.\d+\.\d+\.gem$/)
+
+          expect(objects).to receive(:[]).with(subject.gem_s3_path(gem))
+        end
+      end
+
+      expect(s3_bucket).to(receive(:objects)).
+        at_least(gems.size).times.
+        at_most(gems.size + 2).times. # allow for json, bundler
+        and_return(objects)
+
+      subject.build_and_upload_gems
+    end
+
     it "should have bucket name" do
       expect(subject.bucket_name).to eql 'a-test-bucket'
     end
@@ -21,54 +44,39 @@ module Pipely
       expect(subject.s3_gems_path).to eql 'test_path/gems'
     end
 
+    describe "#gems_from_bundler" do
+      it "should have a hash of gems" do
+        gem_names = subject.gems_from_bundler.keys.reject do |g|
+          g == 'bundler'
+        end.sort
+
+        expect(gem_names).to eql(
+          %w[activesupport aws-sdk aws-sdk-v1 axiom-types builder
+             coercible descendants_tracker equalizer erubis excon fog
+             fog-brightbox fog-core fog-json fog-softlayer formatador
+             i18n ice_nine inflecto ipaddress json mime-types mini_portile
+             minitest multi_json net-scp net-ssh nokogiri rake
+             ruby-graphviz thread_safe tzinfo unf unf_ext uuidtools
+             virtus]
+        )
+      end
+    end
+
     describe "#build_and_upload_gems" do
-      let(:build_and_upload_gems) do
-        VCR.use_cassette('build_and_upload_gems') do
-          subject.build_and_upload_gems
-        end
+      before do
+        build_and_upload_gems
       end
 
       it "should create project gem" do
-        build_and_upload_gems
-
         expect(File).to exist(subject.project_spec.file_name)
-      end
-
-      it "should upload gems" do
-        specs = Bundler.definition.specs_for([:default])
-
-        gems = specs.map(&:file_name).reject do |g|
-          # Ignore bundler, since it could be a system installed gem (travis)
-          # without a cache file
-          g.match(/^json-\d+\.\d+\.\d+\.gem$/) or
-            g.match(/^bundler-\d+\.\d+\.\d+\.gem$/)
-        end
-
-        objects = double(:objects)
-        s3_object = double('s3_object', write: nil)
-
-        allow(objects).to receive(:[]) { s3_object }
-
-        gems.each do |gem|
-          expect(objects).to receive(:[]).with(subject.gem_s3_path(gem))
-        end
-
-        expect(s3_bucket).to(receive(:objects)).
-          at_least(gems.size).times.
-          at_most(gems.size + 2).times. # allow for json, Bundler
-          and_return(objects)
-
-        build_and_upload_gems
       end
     end
 
     describe "#context" do
-      let(:context) { subject.context}
+      let(:context) { subject.context }
 
       before do
-        VCR.use_cassette('build_and_upload_gems') do
-          subject.build_and_upload_gems
-        end
+        build_and_upload_gems
       end
 
       it "should have gem_files" do
@@ -76,9 +84,9 @@ module Pipely
       end
 
       it "should be an s3 url" do
-        expect(context.gem_files.first).to(
-          match( /^s3:\/\/#{subject.bucket_name}\/#{subject.s3_gems_path}/ )
-        )
+        reg =
+          /^s3:\/\/#{subject.bucket_name}\/#{subject.s3_gems_path}/
+        expect(context.gem_files.first).to match(reg)
       end
     end
 
