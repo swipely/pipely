862bc59e99ae92db63d9635d83271bcc651881b6
Keith Barrette <etterrab@gmail.com>
2014-10-27 12:46:04 -0400

    Add s3_dist_cp_hadoop_step

--- spec/lib/pipely/build/template_spec.rb
+++ spec/lib/pipely/build/template_spec.rb
@@ -17,6 +17,25 @@ describe Pipely::Build::Template do
   end
 
   describe "#streaming_hadoop_step(options)" do
+    let(:options) { double(:options) }
+
+    it "builds a streaming hadoop step" do
+      expect(subject).to receive(:hadoop_step).with("/home/hadoop/contrib/streaming/hadoop-streaming.jar", options)
+      subject.streaming_hadoop_step(options)
+    end
+  end
+
+  describe "#s3_dist_cp_hadoop_step(options)" do
+    let(:options) { double(:options) }
+
+    it "builds a S3DistCp hadoop step" do
+      expect(subject).to receive(:hadoop_step).with("/home/hadoop/lib/emr-s3distcp-1.0.jar", options)
+      subject.s3_dist_cp_hadoop_step(options)
+    end
+  end
+
+  describe "#hadoop_step(options)" do
+    let(:jar) { "/home/hadoop/contrib/streaming/hadoop-streaming.jar" }
     let(:s3_path_builder) {
       Pipely::Build::S3PathBuilder.new(
         logs: 'log-bucket',
@@ -30,33 +49,36 @@ describe Pipely::Build::Template do
       subject.apply_config(s3_path_builder.to_hash)
     end
 
-    it "builds a streaming hadoop step" do
-      step = subject.streaming_hadoop_step(
+    it "builds a Hadoop step" do
+      step = subject.hadoop_step(
+        jar,
         :input => '/input_dir/',
         :output => '/output_dir/',
         :mapper => '/mapper.rb',
         :reducer => '/reducer.rb'
       )
 
-      expect(step).to eq("/home/hadoop/contrib/streaming/hadoop-streaming.jar,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,s3n://step-bucket/run-prefix/reducer.rb")
+      expect(step).to eq("#{jar},-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,s3n://step-bucket/run-prefix/reducer.rb")
     end
 
     context "given an array of inputs" do
       it 'points to the IdentityReducer correctly (not as an S3 URL)' do
-        step = subject.streaming_hadoop_step(
+        step = subject.hadoop_step(
+          jar,
           :input => ['/input_dir/', '/input_dir2/'],
           :output => '/output_dir/',
           :mapper => '/mapper.rb',
           :reducer => 'org.apache.hadoop.mapred.lib.IdentityReducer'
         )
 
-        expect(step).to eq("/home/hadoop/contrib/streaming/hadoop-streaming.jar,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir2/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer")
+        expect(step).to eq("#{jar},-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir2/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer")
       end
     end
 
     context "given a cacheFile" do
       it 'points to the IdentityReducer correctly (not as an S3 URL)' do
-        step = subject.streaming_hadoop_step(
+        step = subject.hadoop_step(
+          jar,
           :input => '/input_dir/',
           :output => '/output_dir/',
           :cache_file => '/cache_file#cache_file',
@@ -64,13 +86,14 @@ describe Pipely::Build::Template do
           :reducer => 'org.apache.hadoop.mapred.lib.IdentityReducer'
         )
 
-        expect(step).to eq("/home/hadoop/contrib/streaming/hadoop-streaming.jar,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer,-cacheFile,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/cache_file#cache_file")
+        expect(step).to eq("#{jar},-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer,-cacheFile,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/cache_file#cache_file")
       end
     end
 
     context "given an outputformat" do
       it 'points to the outputformat class (not as an S3 URL)' do
-        step = subject.streaming_hadoop_step(
+        step = subject.hadoop_step(
+          jar,
           :input => '/input_dir/',
           :output => '/output_dir/',
           :outputformat => 'com.swipely.foo.outputformat',
@@ -78,26 +101,28 @@ describe Pipely::Build::Template do
           :reducer => 'org.apache.hadoop.mapred.lib.IdentityReducer'
         )
 
-        expect(step).to eq("/home/hadoop/contrib/streaming/hadoop-streaming.jar,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-outputformat,com.swipely.foo.outputformat,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer")
+        expect(step).to eq("#{jar},-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-outputformat,com.swipely.foo.outputformat,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer")
       end
     end
 
     context "given the IdentityReducer" do
       it 'points to the IdentityReducer correctly (not as an S3 URL)' do
-        step = subject.streaming_hadoop_step(
+        step = subject.hadoop_step(
+          jar,
           :input => '/input_dir/',
           :output => '/output_dir/',
           :mapper => '/mapper.rb',
           :reducer => 'org.apache.hadoop.mapred.lib.IdentityReducer'
         )
 
-        expect(step).to eq("/home/hadoop/contrib/streaming/hadoop-streaming.jar,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer")
+        expect(step).to eq("#{jar},-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer")
       end
     end
 
     context "given jar files" do
       it 'loads the file correctly' do
-        step = subject.streaming_hadoop_step(
+        step = subject.hadoop_step(
+          jar,
           :input => '/input_dir/',
           :output => '/output_dir/',
           :mapper => '/mapper.rb',
@@ -105,13 +130,14 @@ describe Pipely::Build::Template do
           :lib_jars => [ 'filter.jar', 'filter2.jar' ],
         )
 
-        expect(step).to eq("/home/hadoop/contrib/streaming/hadoop-streaming.jar,-libjars,filter.jar,-libjars,filter2.jar,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer")
+        expect(step).to eq("#{jar},-libjars,filter.jar,-libjars,filter2.jar,-input,s3n://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/input_dir/,-output,s3://asset-bucket/run-prefix/\#{format(@scheduledStartTime,'YYYY-MM-dd_HHmmss')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer")
       end
     end
 
     context "given variables" do
       it 'defines them correctly' do
-        step = subject.streaming_hadoop_step(
+        step = subject.hadoop_step(
+          jar,
           :input => '/input_dir/',
           :output => '/output_dir/',
           :mapper => '/mapper.rb',
@@ -119,7 +145,7 @@ describe Pipely::Build::Template do
           :defs => {'mapred.text.key.partitioner.options' => '-k1,1'}
         )
 
-        expect(step).to eq('/home/hadoop/contrib/streaming/hadoop-streaming.jar,-D,mapred.text.key.partitioner.options=-k1\\,1,-input,s3n://asset-bucket/run-prefix/#{format(@scheduledStartTime,\'YYYY-MM-dd_HHmmss\')}/input_dir/,-output,s3://asset-bucket/run-prefix/#{format(@scheduledStartTime,\'YYYY-MM-dd_HHmmss\')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer')
+        expect(step).to eq(jar + ',-D,mapred.text.key.partitioner.options=-k1\\,1,-input,s3n://asset-bucket/run-prefix/#{format(@scheduledStartTime,\'YYYY-MM-dd_HHmmss\')}/input_dir/,-output,s3://asset-bucket/run-prefix/#{format(@scheduledStartTime,\'YYYY-MM-dd_HHmmss\')}/output_dir/,-mapper,s3n://step-bucket/run-prefix/mapper.rb,-reducer,org.apache.hadoop.mapred.lib.IdentityReducer')
       end
     end
   end
