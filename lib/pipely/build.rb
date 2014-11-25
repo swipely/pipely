8c8ca489089307d3a1093554bcfd3654c027eb35
Matt Gillooly <matt@swipely.com>
2014-11-25 14:07:51 -0500

    support config for arbitrary environments

--- lib/pipely/build.rb
+++ lib/pipely/build.rb
@@ -4,6 +4,7 @@ require 'pipely/build/daily_scheduler'
 require 'pipely/build/right_now_scheduler'
 require 'pipely/build/s3_path_builder'
 require 'pipely/build/environment_config'
+require 'pathology'
 
 module Pipely
 
@@ -15,22 +16,7 @@ module Pipely
       env = environment.to_sym
       config = EnvironmentConfig.load(config_path, env)
 
-      case environment.to_sym
-      when :production
-        s3_prefix = "production/#{config[:namespace]}"
-        if config[:start_time]
-          # allow config to change pipelint start time
-          # TODO: all scheduling should be done through config before pipely 1.0
-          scheduler = DailyScheduler.new(config[:start_time])
-        else
-          scheduler = DailyScheduler.new
-        end
-      when :staging
-        s3_prefix = "staging/#{`whoami`.strip}/#{config[:namespace]}"
-        scheduler = RightNowScheduler.new
-      end
-
-      Definition.new(template, env, s3_prefix, scheduler, config)
+      Definition.new(template, env, config)
     end
 
   end
--- spec/lib/pipely/build_spec.rb
+++ spec/lib/pipely/build_spec.rb
@@ -1,3 +1,32 @@
+require 'pipely/build'
+
 describe Pipely::Build do
+
+  describe '.build_definition(template, environment, config_path)' do
+
+    let(:template) { double }
+    let(:environment) { 'production' }
+    let(:config_path) { 'path/to/config' }
+
+    let(:config) { double }
+
+    before do
+      allow(Pipely::Build::EnvironmentConfig).to receive(:load).
+        with(config_path, environment.to_sym).
+        and_return(config)
+    end
+
+    it 'builds a Definition' do
+      expect(
+        described_class.build_definition(template, environment, config_path)
+      ).to eq(
+        Pipely::Build::Definition.new(
+          template,
+          environment.to_sym,
+          config
+        )
+      )
+    end
+  end
 
 end
